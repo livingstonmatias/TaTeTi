1e20ae16d8bffbfb1a4dd3d7bea65382810646e2
_MAL <livingstonmatias@gmail.com>
2020-05-17 01:16:17 -0300

    Updates

---
+++ precache-manifest.7e8b1c140f09c195844515a6481dc513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b170d01df48e9d534ad4d869a46ad88b",
+    "url": "/TaTeTi/index.html"
+  },
+  {
+    "revision": "9324ec02678e801a6a89",
+    "url": "/TaTeTi/static/css/main.e08aee0e.chunk.css"
+  },
+  {
+    "revision": "0107313fef613ed06f78",
+    "url": "/TaTeTi/static/js/2.d464a2c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaTeTi/static/js/2.d464a2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9324ec02678e801a6a89",
+    "url": "/TaTeTi/static/js/main.612a2163.chunk.js"
+  },
+  {
+    "revision": "6ed6893467a28db932df",
+    "url": "/TaTeTi/static/js/runtime-main.28533e25.js"
+  }
+]);
